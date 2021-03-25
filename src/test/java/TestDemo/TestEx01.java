cd2c1f10f0fdcaf74666aa77bfb0f9789ca80e38
viet.thai.le <lethaiviet92@gmail.com>
2021-03-25 23:00:28 +0700

    + Add lib cucumber-java8

--- src/test/java/TestDemo/TestEx01.java
+++ src/test/java/TestDemo/TestEx01.java
@@ -2,10 +2,7 @@ package TestDemo;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import io.cucumber.java8.En;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,7 +11,7 @@ import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class TestEx01 {
+public class TestEx01 implements En {
     private WebDriver driver;
     private final static String CHROME_DRIVER_PATH = "src/test/resources/drivers/chromedriver.exe";
 
@@ -34,35 +31,31 @@ public class TestEx01 {
         }
     }
 
-    @Given("Navigate to Page ForgotPassword")
-    public void navigateToPageForgotPassword() {
-        driver.navigate().to("https://the-internet.herokuapp.com/forgot_password");
-    }
+    TestEx01() {
+        Given("Navigate to Page ForgotPassword", () -> {
+            driver.navigate().to("https://the-internet.herokuapp.com/forgot_password");
+        });
 
-    @When("A User enters a valid email id")
-    public void aUserEntersAValidEmailId() {
-        driver.findElement(By.name("email")).sendKeys("valid@example.com");
-    }
+        When("A User enters a valid email id", () -> {
+            driver.findElement(By.name("email")).sendKeys("valid@example.com");
+        });
 
-    @And("A User clicks on Retrieve password button")
-    public void aUserClicksOnRetrievePasswordButton() {
-        driver.findElement(By.id("form_submit")).click();
-    }
+        When("A User clicks on Retrieve password button", () -> {
+            driver.findElement(By.id("form_submit")).click();
+        });
 
-    @When("A User enters a invalid email id")
-    public void aUserEntersAInvalidEmailId() {
-        driver.findElement(By.name("email")).sendKeys("invalid@");
-    }
+        When("A User enters a invalid email id", () -> {
+            driver.findElement(By.name("email")).sendKeys("invalid@");
+        });
 
-    @Then("Application shows that the email has been sent.")
-    public void applicationShowsThatTheEmailHasBeenSent() {
-        String actualMessage = driver.findElement(By.id("content")).getText();
-        assertThat(actualMessage.trim()).isEqualTo("Your e-mail's been sent!");
-    }
+        Then("Application shows that the email has been sent", () -> {
+            String actualMessage = driver.findElement(By.id("content")).getText();
+            assertThat(actualMessage.trim()).isEqualTo("Your e-mail's been sent!");
+        });
 
-    @Then("Application does not show that email has been sent.")
-    public void applicationDoesNotShowThatEmailHasBeenSent() {
-        String actualMessage = driver.findElement(By.id("content")).getText();
-        assertThat(actualMessage.trim()).isNotEqualTo("Your e-mail's been sent!");
+        Then("Application does not show that email has been sent", () -> {
+            String actualMessage = driver.findElement(By.id("content")).getText();
+            assertThat(actualMessage.trim()).isNotEqualTo("Your e-mail's been sent!");
+        });
     }
 }
--- src/test/resources/features/forgotpassword.feature
+++ src/test/resources/features/forgotpassword.feature
@@ -5,12 +5,10 @@ Feature: Forgot password
     Given Navigate to Page ForgotPassword
     When A User enters a valid email id
     And A User clicks on Retrieve password button
-    Then Application shows that the email has been sent.
+    Then Application shows that the email has been sent
 
   Scenario: An invalid email is not allowed
     Given Navigate to Page ForgotPassword
     When A User enters a invalid email id
     And A User clicks on Retrieve password button
-    Then Application does not show that email has been sent.
-    # TODO - the below step is not be implemented because feature is still not available
-    # And Application shows an error message "Invalid Email"
+    Then Application does not show that email has been sent
