12c343492d86f88f2f7233e8ccfc5b5e340785cc
viet.thai.le <lethaiviet92@gmail.com>
2021-04-01 23:20:37 +0700

    + Add cmd Run Test

---
+++ cmd/RunTest.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+@ECHO Executing: %~f0
+
+SETLOCAL
+SET CURRENT_PATH=%~dp0
+CALL %CURRENT_PATH%Env.bat
+
+PUSHD %WORKSPACE%
+SET CMD=gradlew clean test --tests "RunTests"
+ECHO RUN TEST
+ECHO %CMD%
+CALL %CMD%
+POPD
+
+ENDLOCAL
