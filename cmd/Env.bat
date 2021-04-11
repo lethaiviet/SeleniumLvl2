19d6437e3bbe77dd9aff78e1206a5ea0fce269fe
viet.thai.le <lethaiviet92@gmail.com>
2021-04-11 16:17:16 +0700

    * correct path allure tool

--- cmd/Env.bat
+++ cmd/Env.bat
@@ -4,8 +4,9 @@ SET CURRENT_PATH=%~dp0
 SET WORKSPACE=%CURRENT_PATH%..
 
 SET BUILD_PATH=%WORKSPACE%\build
+SET TOOL_PATH=%WORKSPACE%\tool
 
-SET ALLURE_EXE=%WORKSPACE%\allure-commandline-2.13.9\bin\allure.bat
-SET ALLURE_BIN=%WORKSPACE%\allure-commandline-2.13.9\bin\
+SET ALLURE_EXE=%TOOL_PATH%\allure-commandline-2.13.9\bin\allure.bat
+SET ALLURE_BIN=%TOOL_PATH%\allure-commandline-2.13.9\bin\
 SET ALLURE_RESULTS=%WORKSPACE%\allure-results
 SET ALLURE_REPORT=%WORKSPACE%\allure-report
